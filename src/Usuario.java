6f3dba9e7b73d333bbeda955f3ea98867eee285c
04032059 <enzosilvamelo23@gmail.com>
2019-05-20 20:53:41 -0300

    20/05 a fazer

---
+++ src/Usuario.java
@@ -0,0 +1,15 @@
+public class Usuario {
+    static int usuarioslogados;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void logar(){
+
+    }
+}
